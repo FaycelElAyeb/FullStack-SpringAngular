5d6a003efc472006ad83490a59a84ffb36f2ef54
Faycel El Ayeb <elayebfaycel@gmail.com>
2021-10-03 14:23:46 +0200

    Update

--- src/main/java/com/sip/ams/controllers/EtudiantController.java
+++ src/main/java/com/sip/ams/controllers/EtudiantController.java
@@ -1,9 +1,11 @@
+
 package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +14,8 @@ import org.springframework.web.servlet.ModelAndView;
 import com.sip.ams.entities.Etudiant;
 
 import java.util.*;
-
-@Controller
 @RequestMapping("/etudiant")
+@Controller
 public class EtudiantController {
 	List<Etudiant> etudiants = new ArrayList<>();
 	
@@ -74,34 +75,69 @@ public class EtudiantController {
 		return "redirect:students";
 
 	}
-	
-	
 	@GetMapping("/delete/{ide}")
-	//@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-	public String Suppression(@PathVariable("ide") int id)//id=ide
+	public String suppression(@PathVariable("ide")int id) // id = ide
 	{
-		System.out.println("id= " +id);
-		//on va supprimer ici 
-		
+		System.out.println("id = "+id);
+		// on va supprimer ici
 		Etudiant e = null;
-		e=recherche(etudiants,id);
+		e =  recherche(etudiants, id);
 		etudiants.remove(e);
-		
-		//return e;
 		return "redirect:../students";
 	}
 	
-	private Etudiant recherche(List<Etudiant>le ,int index) {
+	@GetMapping("/update/{ide}")
+	public ModelAndView getUpdateForm(@PathVariable("ide")int id) // id = ide
+	{
+		System.out.println("id = "+id);
+		// on va supprimer ici
+		Etudiant e = null;
+		e =  recherche(etudiants, id);
+		//etudiants.remove(e);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("etudiant", e);
+		mv.setViewName("updateStudent");
+		return mv;
+	}
+	
+	
+	@PostMapping("/update")
+	public String updateEtudiant(Etudiant etudiant) // id = ide
+	{
+		//System.out.println(etudiant);
+		int index = rechercheIndex(etudiants, etudiant);
+		etudiants.set(index, etudiant);
+		return "redirect:students";
+	}
+	
+	
+	private Etudiant recherche(List<Etudiant>le, int index)
+	{
 		Etudiant temp=null;
-		for (Etudiant e : le)
+		for(Etudiant e : le)
 		{
 			if(e.getId()==index)
 			{
-				temp=e;
+				temp = e;
+				return e;
 			}
 		}
 		return temp;
-		
+	}
+	
+	private int rechercheIndex(List<Etudiant> le, Etudiant e)
+	{
+		int compteur = -1;
+		for(Etudiant temp : le)
+		{
+			compteur++;
+			if(temp.getId()==e.getId())
+			{
+				return compteur;
+			}
+			
+		}
+		return compteur;
 	}
 
 }
